250b7f11c2ecd925cdcad99f5428b0d0ac720d8f
Dcoco22 <149657677+Dcoco22@users.noreply.github.com>
2023-12-14 22:07:31 +0700

    Update pt1.php

--- php-lab-2/pt1.php
+++ php-lab-2/pt1.php
@@ -1,1 +1,36 @@
+<html>
+<head>
+<h1> tinh phuong trinh bac mot</h1>
+</head>
+<body>
+<form action="" method="get">
+Input a: <input type=text name=a><br>
+Input b: <input type=text value="" name=b>
+<br>ax + b = 0
+<br><br><input type=submit value="Calculate">
+</form>
+</body>
+<?php
+$a=$_GET["a"];
+$b=$_GET["b"];
+if (isset($a) && isset($b)){
+if ($a!=0)
+{
+$x=-($b)/$a ;
+}
+else
+{
+if ($b==0) {
+$x="vo nghiem";
+}
+else
+{
+$x="vo so nghiem";
+}
+}
+echo "phuong trinh co nghiem x = ". $x;
+}
+?>
+</html>
+
 
