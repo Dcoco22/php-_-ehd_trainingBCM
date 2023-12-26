cf75529b04753be0a9f8900719204158c016c4f6
Dcoco22 <149657677+Dcoco22@users.noreply.github.com>
2023-12-26 22:25:55 +0700

    Update pt1.php

--- php-lab-4/pt1.php
+++ php-lab-4/pt1.php
@@ -1,1 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+            <b><label>Upload an image</label></b><br>   
+            <input type="file" id="myFile" name="filename"><br>
+            <input type="submit" name='sub'><br>
+    </form>
 
+<?php
+if (isset($_POST['sub'])) {
+    if (isset($_FILES['filename'])) {
+        $filepath = "" . $_FILES["filename"]["name"];
+        if (move_uploaded_file($_FILES["filename"]["tmp_name"], $filepath)) {
+             echo "<div style='text-align: center; '>";
+            echo "<img src=" . $filepath . " />";
+            echo "</div>";        } else {
+            echo "Upload unsuccessfully";
+        }
+    }
+}
+?>
+</body>
+</html>
